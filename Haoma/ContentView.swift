4b343dbfced938f9f2e6b6bbff9c4171eea8c3ad
Bahram <bahram.salik1@gmail.com>
2025-07-15 15:16:31 -0400

    first commit

--- Haoma/ContentView.swift
+++ Haoma/ContentView.swift
@@ -9,14 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            EntryForm()
         }
         .padding()
     }
+}
+
+struct EntryForm: View {
+  @State var valueStatement = ""
+  @State var bodyStatement = ""
+  @State var reasons = ["","",""]
+  @State var supportive = ["","",""]
+  @State var obstructive = ["","",""]
+  @State var mood = 5
+  // …
+  var body: some View {
+    Form {
+      Section("Value for Today") {
+        TextField("What life do I want?", text: $valueStatement)
+        TextField("What body do I want?", text: $bodyStatement)
+      }.padding(10)
+      Section("Supportive Behaviors") {
+        ForEach(0..<3) { i in
+          TextField("\(i+1)", text: $supportive[i])
+        }.padding(2)
+      }
+      Section("Obstructive Behaviours") {
+        ForEach(0..<3) { i in
+            TextField("\(i+1)", text: $obstructive[i])
+        }.padding(2)
+      }
+        Section("What are three reasons you can succeed today?") {
+          ForEach(0..<3) { i in
+              TextField("\(i+1)", text: $reasons[i])
+          }.padding(2)
+        }
+      // … repeat for obstructive, mood slider, etc.
+        Button("Save") { saveEntry()}.padding(30)
+    }
+  }
+}
+
+func saveEntry() {
+    
 }
 
 #Preview {
