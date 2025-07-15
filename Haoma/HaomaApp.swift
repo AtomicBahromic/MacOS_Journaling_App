7acd284e38b6271ac977d097d6390625d2a94f56
Bahram <bahram.salik1@gmail.com>
2025-07-15 14:31:37 -0400

    Initial Commit

---
+++ Haoma/HaomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaomaApp.swift
+//  Haoma
+//
+//  Created by Bahram Salik on 2025-07-15.
+//
+
+import SwiftUI
+
+@main
+struct HaomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HaomaTests/HaomaTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaomaTests.swift
+//  HaomaTests
+//
+//  Created by Bahram Salik on 2025-07-15.
+//
+
+import Testing
+@testable import Haoma
+
+struct HaomaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
